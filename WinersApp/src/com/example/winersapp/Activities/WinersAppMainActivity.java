510918dde696157f28b39962b6f18d7e4306c631
Victoria Do <veeqdee@gmail.com>
2013-05-04 12:48:24 -0700

    Separated login screen into its own file.
    * Added a button style and border around the login window.
    * Used an eclipse-generated template for the login window - resulting in a lot of
      extraneous code.

--- WinersApp/src/com/example/winersapp/Activities/WinersAppMainActivity.java
+++ WinersApp/src/com/example/winersapp/Activities/WinersAppMainActivity.java
@@ -2,12 +2,17 @@ package com.example.winersapp.Activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.winersapp.R;
@@ -17,6 +22,8 @@ import com.example.winersapp.R.layout;
 import com.example.winersapp.R.menu;
 
 public class WinersAppMainActivity extends Activity {
+	
+	PopupWindow loginWindow;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,16 +58,23 @@ public class WinersAppMainActivity extends Activity {
         return true;
     }
     
-    /* Use Case: Register Window
-     * Controls opening and closing the Login windows. 
-     * Author: Victoria
+    /* Use Case: Login Window
+     * Controls opening and closing the Login window.
      */
     public void showLogin(View view) {
-    	findViewById(R.id.loginWindow).setVisibility(0);
+    	// Instantiate PopupWindow containing the login layout.
+    	LayoutInflater inflater = (LayoutInflater)
+    		       this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);    	
+    	loginWindow = new PopupWindow(
+    			inflater.inflate(R.layout.activity_login, null, false), 
+    			LayoutParams.WRAP_CONTENT,
+    			LayoutParams.WRAP_CONTENT,
+    			true);
+    	loginWindow.showAtLocation(this.findViewById(R.id.main), Gravity.CENTER, 0, 0); 
     }
     
     public void closeLogin(View view) {
-    	findViewById(R.id.loginWindow).setVisibility(8);
+    	loginWindow.dismiss();
     }
     
    
