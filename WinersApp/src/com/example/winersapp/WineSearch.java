e4af37071ccf79279fcc82c1f972d128ba987cb4
Hesler Rodriguez <hesler10@gmail.com>
2013-05-05 19:46:33 -0700

    Fixed the wine search bug. works fine now

--- WinersApp/src/com/example/winersapp/WineSearch.java
+++ WinersApp/src/com/example/winersapp/WineSearch.java
@@ -5,12 +5,20 @@ import com.example.winersapp.Activities.AbstractActivity;
 import com.example.winersapp.Activities.MainActivity;
 
 import android.app.Activity;
+import android.app.ExpandableListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class WineSearch extends AbstractActivity {
+public class WineSearch extends ExpandableListActivity {
+	
+	private String[] categories 	= {"Color", "Season", "Type", "Accent"};
+	private String[] colors 		= {"Red", "White", "Rose", "Sparkling"};
+	private String[] seasons 		= {"Fall", "Winter", "Spring", "Summer"};
+	private String[] types 			= {"Chardonnay","Sauvignon Blanc", "Pinot Gris",
+							  		   "Merlot", "Shiraz", "Cabernet Sauvignon"};
+	private String[] accents 		= {"Acidic", "Acrid", "Ageworthy", "Aggressive"}; 
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,9 @@ public class WineSearch extends AbstractActivity {
 			}
         });
 	}
+	
+	
 
-	@Override
 	protected int getTitleText() {
 		// TODO Auto-generated method stub
 		return R.string.wine_search_title;
