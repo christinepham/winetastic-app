23957508c78710f7d172ea657af9dec476610586
Helena Qin <helena.qin@gmail.com>
2013-05-28 17:59:40 -0700

    Added the "Daily Vine" so that when the user clicks on the bookmark button on the Home Page, a random winery/wine is shown.

--- WinersApp/src/com/winers/winetastic/WineryOfDay.java
+++ WinersApp/src/com/winers/winetastic/WineryOfDay.java
@@ -6,23 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
 import android.widget.TextView;
-
-import com.google.gson.Gson;
 
 import com.winers.winetastic.R;
 
-public class WineOfDay extends Fragment {	
+public class WineryOfDay extends Fragment {
 	
-	public WineOfDay() {
+	public WineryOfDay() {
 		
 	}
-
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = (View) inflater.inflate(R.layout.fragment_wine_of_day, container, false);
+        View rootView = (View) inflater.inflate(R.layout.fragment_winery_of_day, container, false);
         return rootView;
     }
-
 }
